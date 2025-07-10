a14d6cc9c517dd50f39882432c46b946c275e724
ezequielbeider <eze-2001@hotmail.com>
2025-07-09 21:09:32 -0300

    primero

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// Cambiar navbar al hacer scroll
+window.addEventListener('scroll', function() {
+  const navbar = document.querySelector('.navbar');
+  navbar.classList.toggle('scrolled', window.scrollY > 50);
+});
+
+
